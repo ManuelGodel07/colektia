475bad0ebfd1e6444c9e71d56ca97d86bfa5a290
unknown <manuel_ruiz@ciencias.unam.mx>
2024-10-23 06:37:53 -0600

    seventh commit

---
+++ src/assets/trainQuestions.js
@@ -0,0 +1,57 @@
+export const trainSet = [
+    {
+        questionText: "¿Consideras que la capacitación fue clara y bien estructurada?",
+        name: "1",
+        options: [
+            { choice: "a) Si", radioValue: "q1-a", selected: false },
+            { choice: "b) Mas o menos", radioValue: "q1-a", selected: false },
+            { choice: "c) No", radioValue: "q1-a", selected: false }
+        ]
+    },
+    {
+    questionText: "¿La capacitación cubrió todas las habilidades y conocimientos necesarios para tu rol?",
+    name: "2",
+    options: [
+        { choice: "a) Si", radioValue: "q2-a", selected: false },
+        { choice: "b) Mas o menos", radioValue: "q2-b", selected: false },
+        { choice: "c) No", radioValue: "q2-c", selected: false } ]
+    },
+    {
+    questionText: "¿Los instructores o facilitadores fueron efectivos y respondieron tus dudas adecuadamente?",
+    name: "3",
+    options: [
+        { choice: "a) Si", radioValue: "q3-a", selected: false },
+        { choice: "b) Mas o menos", radioValue: "q3-b", selected: false },
+        { choice: "c) No", radioValue: "q3-c", selected: false }]
+    },
+    {
+    questionText: "¿Recibiste suficiente tiempo y apoyo para practicar lo aprendido durante la capacitación?",
+    name: "4",
+    options: [
+        { choice: "a) Si", radioValue: "q4-a", selected: false },
+        { choice: "b) No", radioValue: "q4-b", selected: false }
+    ]
+    },
+    {
+        questionText: "¿Qué tan fácil te fue aplicar tus conocimientos y habilidades a la capacitación",
+        name: "5",
+        options: [
+            { choice: "a) Buena", radioValue: "q5-a", selected: false },
+            { choice: "b) Regular", radioValue: "q5-b", selected: false },
+            { choice: "c) Mala", radioValue: "q5-c", selected: false }]
+    },
+    {
+        questionText: "¿Consideras que la duración de la capacitación fue adecuada para cubrir todos los temas?",
+        name: "6",
+        options: [
+            { choice: "a) Si", radioValue: "q6-a", selected: false },
+            { choice: "a) No", radioValue: "q6-b", selected: false }]
+    },
+    {
+        questionText: "¿Te sientes list para realizar tus tareas con éxito después de la capacitación?",
+        name: "7",
+        options: [
+            { choice: "a) Si", radioValue: "q7-a", selected: false },
+            { choice: "b) No", radioValue: "q7-b", selected: false } ]
+    }
+];
--- src/screens/Training.js
+++ src/screens/Training.js
@@ -1,9 +1,140 @@
-import React from 'react'
+import { useState } from "react";
+import { collection, addDoc } from "firebase/firestore";
+import { db } from "../config/firestore";
+import { trainSet } from "../assets/trainQuestions";
+
 
 const Training = () => {
-  return (
-    <div>Training</div>
-  )
+const [name, setName] = useState('');
+const [questions, setQuestions] = useState(trainSet);
+const [selectedValues, setSelectedValues] = useState({});
+const [selectedOption, setSelectedOption] = useState("");
+const [salida, setSalida] = useState("");
+
+
+const handleChange = (event, questionName) => {
+    const target = event.target;
+    const newValue = event.target.getAttribute('data-value2');
+    console.log('new:', newValue)
+    const selectedQuestion = questions.find((question) => question.name === target.name);
+
+    const selectedOption = selectedQuestion.options.find((opt) => opt.radioValue === target.value);
+
+    setSelectedValues((prevValues) => ({
+    ...prevValues,
+    [questionName]: newValue,
+    }));
+    console.log(`Pregunta: ${questionName}, Valor seleccionado: ${selectedOption}`);
+    const nextState = questions.map((question) => {
+    if (question.name !== target.name) {
+        return question;
+    }
+
+    return {
+        ...question,
+        options: question.options.map((opt) => {
+        const checked = opt.radioValue === target.value;
+        return {
+            ...opt,
+            selected: checked,
+        };
+        }),
+        currentAnswer: target.value,
+    };
+    });
+    setQuestions(nextState);
+};
+const handleInputText = (event,actualizador) => {
+    const newValue = event.target.value;
+    actualizador((prevValues) => ({
+    ...prevValues,
+    'Salida': newValue,
+    }));
+};
+
+const handleName =(e)=>{
+    const myName = e.target.value;
+    setName(myName)
+    setSelectedValues((prevValues) => ({
+        ...prevValues,
+        'Name': myName,
+    }));
+}
+
+const saveInfo= async()=>{
+    const clavesEsperadas = [ '3','4','5','6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20', '21', '22'];
+
+    const clavesFaltantes = clavesEsperadas.filter(clave => 
+    !selectedValues.hasOwnProperty(clave) || selectedValues[clave] === undefined || selectedValues[clave] === ''
+    );
+
+    if (clavesFaltantes.length > 0) {
+    console.log(selectedValues);
+    alert(`Faltan las siguientes respuestas: ${clavesFaltantes.join(', ')}`);
+    } else {
+    console.log(selectedValues);
+        try {
+        await addDoc(collection(db,'employees'),{
+        ...selectedValues
+        })
+        console.log('-',selectedValues)
+        setSelectedValues({})
+        } catch (error) {
+            console.log(error)
+        }
+    alert('Todas las respuestas están presentes.');
+    }
+}
+
+return (
+    <div className="App-main">
+    <div className="Header-container">
+        <h1 >Encuesta de satisfacción</h1>
+        <h2>¡Tu opinión cuenta! Ayúdanos a mejorar.</h2>
+        <h3>Responde nuestra encuesta de satisfacción y ayúdanos a identificar áreas de mejora en la empresa</h3>
+    </div>
+    <section>
+        <label>Escribe tu nombre completo:
+        <input type="text" value={name} onChange={(event)=>handleName(event)}/>
+        </label>
+        {questions.map((question, idx) => (
+        <div key={`group-${idx}`}>
+            <h3>
+            {idx + 1}. {question.questionText}
+            </h3>
+            {question.options.map((option, idx) => {
+            return (
+                <div key={`option-${idx}`}>
+                <input
+                    type="radio"
+                    name={question.name}
+                    value={option.radioValue}
+                    data-value2={option.choice}
+                    checked={option.selected}
+                    onChange={(event) => handleChange(event, question.name)} 
+                />
+                {option.choice}
+                </div>
+            );
+            })}
+        </div>
+        ))}
+        <div>
+            <h3>8. ¿Qué partes de la capacitación encontraste más útiles y cuáles menos relevantes?</h3>
+            <textarea
+            value={salida.Salida}
+            onChange={(event) => handleInputText(event, setSelectedValues)}
+            rows="5"
+            cols="80"
+            placeholder="Escribe aquí..."
+            />
+        </div>
+        <div>
+        <button className="sendInfo" onClick={saveInfo} >Enviar</button>
+        </div>
+    </section>
+    </div>
+);
 }
 
 export default Training;
